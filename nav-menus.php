6f039f18cdb2e7e965102d7ffb12b049cd9bc508
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-03-26 12:01:38 +0000

    update www/semiologic.com/software urls
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/nav-menus/trunk@961 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- nav-menus.php
+++ nav-menus.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Nav Menus
-Plugin URI: http://www.semiologic.com/software/widgets/nav-menus/
+Plugin URI: http://www.semiologic.com/software/nav-menus/
 Description: WordPress widgets that let you create navigation menus
 Version: 1.2.2 alpha
 Author: Denis de Bernardy
