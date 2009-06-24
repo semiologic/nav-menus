c6ad1506a16a2b362d111bbb08dfc03e7734ee80
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-06-24 21:28:09 +0000

    add missing widget_title filters
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/nav-menus/trunk@1827 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- nav-menus.php
+++ nav-menus.php
@@ -156,7 +156,7 @@ class nav_menu extends WP_Widget {
 		if ( is_admin() ) {
 			echo $before_widget
 				. ( $title
-					? ( $before_title . $title . $after_title )
+					? ( $before_title . apply_filters('widget_title', $title) . $after_title )
 					: ''
 					)
 				. $after_widget;
@@ -197,7 +197,7 @@ class nav_menu extends WP_Widget {
 		echo $before_widget;
 		
 		if ( $title )
-			echo $before_title . $title . $after_title;
+			echo $before_title . apply_filters('widget_title', $title) . $after_title;
 		
 		echo '<ul>' . "\n";
 		
