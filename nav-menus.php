c9edb292b4dd4edbe350f3ab9b63accb2baf9a60
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-01 15:02:59 +0000

    work around sidebars_widgets bugs
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/nav-menus/trunk@1897 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- nav-menus.php
+++ nav-menus.php
@@ -1081,11 +1081,13 @@ class nav_menu extends WP_Widget {
 			}
 		}
 		
-		global $wp_filter;
+		global $wp_filter, $_wp_sidebars_widgets;
 		$filter_backup = isset($wp_filter['sidebars_widgets']) ? $wp_filter['sidebars_widgets'] : array();
 		unset($wp_filter['sidebars_widgets']);
+		$_wp_sidebars_widgets = array();
 		$sidebars_widgets = wp_get_sidebars_widgets(false);
 		$wp_filter['sidebars_widgets'] = $filter_backup;
+		$_wp_sidebars_widgets = array();
 		
 		$keys = array_keys($extra);
 		
@@ -1102,8 +1104,6 @@ class nav_menu extends WP_Widget {
 		}
 		
 		wp_set_sidebars_widgets($sidebars_widgets);
-		global $_wp_sidebars_widgets;
-		$_wp_sidebars_widgets = array();
 		
 		return $ops;
 	} # upgrade()
