294a91163cb4cc92b4dbc39c113eefc9cb60edcd
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-09 07:38:02 +0000

    use the correct hook for cache flushes
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/nav-menus/trunk@2012 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- nav-menus.php
+++ nav-menus.php
@@ -54,7 +54,7 @@ foreach ( array(
 		'generate_rewrite_rules',
 		
 		'flush_cache',
-		'update_option_db_version',
+		'after_db_upgrade_version',
 		) as $hook)
 	add_action($hook, array('nav_menu', 'flush_cache'));
 
