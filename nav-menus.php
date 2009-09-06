54716b5954bd1dacf447cb7f7ca72437cd1a2060
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-09-06 08:55:41 +0000

    add missing permalink filters
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/nav-menus/trunk@2520 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- nav-menus.php
+++ nav-menus.php
@@ -359,7 +359,7 @@ class nav_menu extends WP_Widget {
 		if ( (string) $label === '' )
 			$label = __('Untitled', 'nav-menus');
 		
-		$url = esc_url(get_permalink($page->ID));
+		$url = esc_url(apply_filters('the_permalink', get_permalink($page->ID)));
 		
 		$ancestors = $page_id ? wp_cache_get($page_id, 'page_ancestors') : array();
 		$children = wp_cache_get($page->ID, 'page_children');
